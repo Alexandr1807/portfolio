6c528ea5d3ca85fad784d03cd60d3b959fe3a4e9
Alexandr1807 <alexandrcyganov18@gmail.com>
2023-11-22 01:04:34 +0500

    changes for another deploy

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,21 +6,21 @@ const items = [
   {
     id: 1,
     title: "VSHN",
-    img: "/vshn.png",
+    img: "vshn.png",
     desc: "VSHN is the Ops in DevOps. We automate the operation of applications in the cloud or on-premise, so that software developers can focus on their business.",
     link: "https://alexandr1807.github.io/VSHN/"
   },
   {
     id: 2,
     title: "Shelter",
-    img: "/portfolio1.png",
+    img: "portfolio1.png",
     desc: "We offer to give a chance to a little and nice puppy with an extremely wide and open heart. He or she will love you more than anybody else in the world, you will see!",
     link: "https://alexandr1807.github.io/Shelter/"
   },
   {
     id: 3,
     title: "Virtual keyboard",
-    img: "/vb.png",
+    img: "vb.png",
     desc: "My virtual keayboard",
     link: 'https://alexandr1807.github.io/virtual-keyboard/'
   },
